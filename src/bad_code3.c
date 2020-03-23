6af942d7886ba327d2adfce0a306963fe9da33b6
hark130 <hark130@yahoo.com>
2020-03-23 12:00:22 -0500

    valgrind: Added a memory leak example
    Changes to be committed:
    	new file:   src/bad_code3.c
    	new file:   src/good_code3.c

---
+++ src/bad_code3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>          // printf()
+#include <stdlib.h>         // free()
+#include <string.h>         // strcpy()
+#include "gimme_mem.h"      // gimme_mem_malloc()
+
+#define BUF_SIZE 64
+
+
+int main(void)
+{
+    /***********************************************************/
+    /* 3. Memory Leak                                          */
+    /***********************************************************/
+    // LOCAL VARIABLES
+    char *bufLeak = (char *)gimme_mem_malloc((BUF_SIZE * sizeof(char)) + 1);
+
+    // COPY
+    strcpy(bufLeak, "Hello World!\n");
+
+    // PRINT
+    printf("%s", bufLeak);
+
+    // CLEAN UP
+    // free(bufLeak);
+
+    // DONE
+    return 0;
+}
