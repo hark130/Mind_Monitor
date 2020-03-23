a0d7634549a2eda2dd0874b1734c4ca4b7fca1b3
hark130 <hark130@yahoo.com>
2020-03-23 16:18:29 -0500

    mtrace: MIMO WRAPPERS conditional linking
    This header should be enough to handle whatever special calls need to be
    made.  The object that the binaries will be linked to behind the scenes
    will define different implementations of the functions declared here.
    The makefile recipes will handle conditional linking.
    
    Changes to be committed:
    	new file:   src/mimo_wrappers.h
    
    Untracked files:
    	src/mimo_wrappers.c
    	src/mimo_wrappers_mtrace.c

---
+++ src/mimo_wrappers.h
@@ -0,0 +1,19 @@
+#ifndef __MIMO_WRAPPERS_H__
+#define __MIMO_WRAPPERS_H__
+
+
+/*
+ * Purpose - Modular wrapper to handle debugger setup with minimal code
+ *	change.
+ */
+void setup_mimo(void);
+
+
+/*
+ * Purpose - Modular wrapper to handle debugger teardown with minimal code
+ *	change.
+ */
+void teardown_mimo(void);
+
+
+#endif  // __MIMO_WRAPPERS_H__
