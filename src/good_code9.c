d6ea5b5d8a4e4083cb5a0c1fec63a3dcbcddf88e
hark130 <hark130@yahoo.com>
2020-03-23 18:04:48 -0500

    mtrace: Implemented MIMO_W in remaining code
    Implemented the MIMO Wrappers in the rest of the code base.
    Now mtrace is working with all the existing code.
    Valgrind is still working as before.
    Mtrace has found 1 of 9 errors.
    
    Changes to be committed:
    	modified:   src/good_code4.c
    	modified:   src/good_code5.c
    	modified:   src/good_code6.c
    	modified:   src/good_code7.c
    	modified:   src/good_code8.c
    	modified:   src/good_code9.c

--- src/good_code9.c
+++ src/good_code9.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>         // free()
 #include <string.h>         // strcpy()
 #include "gimme_mem.h"      // gimme_mem_malloc()
+#include "mimo_wrappers.h"  // setup_mimo(), teardown_mimo()
 
 #define BUF_SIZE 64
 
@@ -11,6 +12,9 @@ int main(void)
     /***********************************************************/
     /* 9. Fishy values passed into calloc                      */
     /***********************************************************/
+    // SETUP
+    setup_mimo();
+
     // LOCAL VARIABLES
     char *bufFishyCalloc = (char *)gimme_mem_calloc((BUF_SIZE * sizeof(char)) + 1);
 
@@ -24,5 +28,6 @@ int main(void)
     free(bufFishyCalloc);
 
     // DONE
+    teardown_mimo();
     return 0;
 }
