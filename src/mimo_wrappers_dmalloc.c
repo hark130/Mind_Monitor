3ecbe848a64ec16d00189e608ac55097ef57043b
hark130 <hark130@yahoo.com>
2020-03-24 12:11:53 -0500

    dmalloc: MIMO Wrapper for Dmalloc initial commit
    Apparently, all dmalloc needs is the header... No function call.
    "Let us see." - Mrs. Harkleroad
    
    Changes to be committed:
    	new file:   src/mimo_wrappers_dmalloc.c

---
+++ src/mimo_wrappers_dmalloc.c
@@ -0,0 +1,14 @@
+#include <dmalloc.h>
+#include "mimo_wrappers.h"
+
+
+void setup_mimo(void)
+{
+    return;  // Default behavior
+}
+
+
+void teardown_mimo(void)
+{
+    return;  // Default behavior
+}
