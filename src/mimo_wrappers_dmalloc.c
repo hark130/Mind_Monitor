48103dd6f3d0ce4abb3560a22d734210d08d15d3
hark130 <hark130@yahoo.com>
2020-03-25 10:45:46 -0500

    dmalloc: Trying to improve Dmalloc output Part 2
    Added call to dmalloc_shutdown() in teardown_mimo() as defined in
    mimo_wrappers_dmalloc.c.  I don't think it helped but I'm leaving it in.
    
    Changes to be committed:
    	modified:   src/mimo_wrappers_dmalloc.c

--- src/mimo_wrappers_dmalloc.c
+++ src/mimo_wrappers_dmalloc.c
@@ -1,5 +1,5 @@
-#include <dmalloc.h>
 #include "mimo_wrappers.h"
+#include <dmalloc.h>
 
 
 void setup_mimo(void)
@@ -10,5 +10,12 @@ void setup_mimo(void)
 
 void teardown_mimo(void)
 {
+    /*
+     *  This function shuts the library down and logs the final statistics and
+     *  information especially the non-freed memory pointers.
+     *
+     *  From: https://dmalloc.com/docs/5.3.0/online/dmalloc_13.html
+     */
+    dmalloc_shutdown();
     return;  // Default behavior
 }
