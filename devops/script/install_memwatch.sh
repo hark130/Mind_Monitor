9387ce71ba5abef49f67da52dded3c413ddd5391
hark130 <hark130@yahoo.com>
2020-03-26 14:19:46 -0500

    memwatch: install_memwatch.sh Initial Commit
    Wrote a basic script to install the Memwatch files
    
    Changes to be committed:
    	new file:   devops/script/install_memwatch.sh

---
+++ devops/script/install_memwatch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -P ~/Downloads/ https://www.linkdata.se/downloads/sourcecode/memwatch/memwatch-2.71.tar.gz
+tar -xvf ~/Downloads/memwatch-2.71.tar.gz -C ~/Downloads/
+cp ~/Downloads/memwatch-2.71/memwatch.? src/
