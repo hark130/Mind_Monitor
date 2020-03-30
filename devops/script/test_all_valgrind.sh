992e2a7899b396cc2e94e2654350bb09d7529e62
hark130 <hark130@yahoo.com>
2020-03-30 16:22:33 -0500

    bad_code10: Updated valgrind script
    I was trying to "massage" the Valgrind script into picking up this
    multi-process memory leak but I couldn't.
    However, I added a switch which should aid Valgrind in getting to the
    multi-threaded memory leak so I'm leaving it in.
    
    Changes to be committed:
    	modified:   devops/script/test_all_valgrind.sh

--- devops/script/test_all_valgrind.sh
+++ devops/script/test_all_valgrind.sh
@@ -104,7 +104,7 @@ do
     # $TEMP_REL_FILENAME 2>&1 /dev/null
     # B. Silence the valgrind errors
     valgrind -q --leak-check=full --track-origins=yes --tool=memcheck\
-    --child-silent-after-fork=yes --error-exitcode=1\
+    --child-silent-after-fork=yes --error-exitcode=1 --trace-children=yes\
     $TEMP_REL_FILENAME > /dev/null 2>&1
 
     # Check the results
