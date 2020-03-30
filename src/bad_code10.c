743b27ba900e4d14702f82562c812fca8cdbd7b8
hark130 <hark130@yahoo.com>
2020-03-30 15:18:50 -0500

    bad_code10: Good/Bad Code 10 Initial Commit
    Changes to be committed:
    	new file:   src/bad_code10.c
    	new file:   src/good_code10.c

---
+++ src/bad_code10.c
@@ -0,0 +1,62 @@
+#include <sys/types.h>      // pid_t
+#include <stdio.h>          // fprintf()
+#include <stdlib.h>         // exit(), free()
+#include <string.h>         // strcpy()
+#include <unistd.h>         // fork()
+#include "gimme_mem.h"      // gimme_mem_malloc()
+#include "mimo_wrappers.h"  // setup_mimo(), teardown_mimo()
+
+
+/*
+ *  Purpose - Encapsulate all the work the child process will do
+ */
+int execute_child(void);
+
+
+int main(void)
+{
+    /***********************************************************/
+    /* 10. Multi-Threaded Memory Leak                          */
+    /***********************************************************/
+    // LOCAL VARIABLES
+    pid_t pid = 0;  // Process IDentifier
+
+    // PARENT WORK
+    // Fork off the parent process
+    pid = fork();
+    if (pid < 0)
+    {
+        fprintf(stderr, "PARENT: Failed to fork!");
+        exit(EXIT_FAILURE);  // Error
+    }
+    else if (pid > 0)
+    {
+        fprintf(stdout, "PARENT: Fork successful");
+        exit(EXIT_SUCCESS);  // Child process successfully forked
+    }
+
+    execute_child();
+}
+
+
+int execute_child(void)
+{
+    // SETUP
+    setup_mimo();
+
+    // LOCAL VARIABLES
+    char *bufLeak = (char *)gimme_mem_malloc((BUF_SIZE * sizeof(char)) + 1);
+
+    // COPY
+    strcpy(bufLeak, "Hello World!\n");
+
+    // PRINT
+    printf("%s", bufLeak);
+
+    // CLEAN UP
+    // free(bufLeak);
+
+    // DONE
+    teardown_mimo();
+    exit(EXIT_SUCCESS);
+}
